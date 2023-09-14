d67b4d24a84b9bbd0dbb5b44b474092dde135dc3
Arda <111998578+gokcearda@users.noreply.github.com>
2023-09-14 04:56:35 +0300

    .

--- flightsearchapi/repository/AirportRepository.java
+++
@@ -1,5 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.flightsearchapi.model.Airport;
-
-public interface AirportRepository extends JpaRepository<Airport, Long> {
-}
--- flightsearchapi/src/main/java/com/example/flightsearchapi/repository/FlightRepository.java
+++ flightsearchapi/src/main/java/com/example/flightsearchapi/repository/FlightRepository.java
@@ -1,5 +1,7 @@
-import org.springframework.data.jpa.repository.JpaRepository;
+package com.example.flightsearchapi.repository;
+
 import com.example.flightsearchapi.model.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
 }
